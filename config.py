50175d96711a93960b9606be03a5f08f90109148
risethecode <dayarmustfa2@gmail.com>
2026-02-12 16:52:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "23031620"))
-API_HASH = getenv("API_HASH", "31cb00c1cbe580394778b43105864bca")
-BOT_TOKEN = getenv("BOT_TOKEN", "7035220759:AAEQcpQXZOd8aWgJkXQMmUzoeFuU_ufYego")
-OWNER_ID = int(getenv("OWNER_ID", "502980590"))
+API_ID = int(getenv("API_ID", "22518279"))
+API_HASH = getenv("API_HASH", "61e5cc94bc5e6318643707054e54caf4")
+BOT_TOKEN = getenv("BOT_TOKEN", "8006527414:AAEZvtQy5aWXrnRxBi_vaVTUg2f-tQLoMXc")
+OWNER_ID = int(getenv("OWNER_ID", "8500852075"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "502980590").split()))
-MONGO_URL = mongodb+srv://altafpathan65012:<password>@cluster0.aztmome.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = "mongodb+srv://<sandeepsharma786>:<Sandeep786>@cluster0.xz78w44.mongodb.net")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002155787742"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002155787742"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003828380273"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1003533276261"))
 
 """
 #
@@ -24,7 +24,7 @@ import os
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("APP_ID", 23031620))
+API_ID = int(os.environ.get("APP_ID", 22518279))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH")
 # ----------------D--------------------------------
@@ -32,12 +32,12 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "502980590"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8500852075"))
 # ------------------X------------------------------
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "502980590").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002155787742"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003828380273"))
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002155787742"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003533276261"))
